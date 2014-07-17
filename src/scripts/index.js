b8bd92bde455225f9b6e5924d59c864fc32fc929
ZauberNerd <zaubernerd@zaubernerd.de>
2014-07-17 20:34:29 +0200

    added ability to share code via hash

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,7 +14,7 @@ function tryCatch(fn, ctx, args) {
 function compile(code) {
     var result = tryCatch(compiler.compile, compiler, [code]);
     if(result === errorObject) {
-        return { error: errorObject.value };
+        return { error: errorObject.value, result: null };
     } else {
         return { error: null, result: result };
     }
@@ -33,14 +33,7 @@ function formatError(err) {
     return 'Compile Error: (' + loc + '): ' + err.message;
 }
 
-
-
-var textarea = document.getElementById('input');
-var warnings = document.getElementById('warnings');
-var trigger  = document.querySelectorAll('.trigger');
-var pre      = document.getElementById('output');
-
-textarea.addEventListener('keyup', delay(function () {
+function onKeyUp() {
     pre.innerText = warnings.innerText = '';
     var result = compile(textarea.value);
 
@@ -49,10 +42,34 @@ textarea.addEventListener('keyup', delay(function () {
     } else {
         warnings.innerText = formatError(result.error);
     }
-}, 200), false);
+}
 
-Array.prototype.slice.call(trigger).forEach(function (el) {
+function addPanelClickListeners(el) {
     el.addEventListener('click', function () {
         el.parentNode.classList.toggle('open');
     }, false);
-});
+}
+
+function shareCode() {
+    var url = location.protocol + '//' + location.host + location.pathname;
+    copyLink.value = url + '#' + encodeURIComponent(textarea.value);
+    copyLink.classList.toggle('show');
+    copyLink.select();
+    copyLink.focus();
+}
+
+
+var textarea = document.getElementById('input');
+var warnings = document.getElementById('warnings');
+var pre      = document.getElementById('output');
+var share    = document.getElementById('share-code');
+var copyLink = document.getElementById('copy-link');
+var trigger  = document.querySelectorAll('.trigger');
+var code     = decodeURIComponent(location.hash.substr(1));
+
+textarea.value = code;
+onKeyUp();
+
+textarea.addEventListener('keyup', delay(onKeyUp, 200), false);
+share.addEventListener('click', shareCode, false);
+Array.prototype.slice.call(trigger).forEach(addPanelClickListeners);
